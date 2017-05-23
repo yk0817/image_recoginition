4a8fdddf7517ea5022d0c0663e0f71e6aaad0611
山本憲 <em072029@gmail.com>
2017-05-23 22:01:13 +0900

    utf8

--- ,open_test01.py
+++ ,open_test01.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import cv2
 import numpy as np
 
@@ -7,13 +8,15 @@ while(1):
 
     _, frame = cap.read()
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    print(hsv)
     
-    #色の下限、上限に応じて出力を調整している。
+    # u"色の下限、上限に応じて出力を調整している。"
     # lower_red = np.array([30,150,50])
     lower_red = np.array([0,0,0])
     # upper_red = np.array([255,255,180])
     upper_red = np.array([255,255,255])
     
+    # 
     mask = cv2.inRange(hsv, lower_red, upper_red)
     res = cv2.bitwise_and(frame,frame, mask= mask)
     kernel = np.ones((15,15),np.float32)/225
