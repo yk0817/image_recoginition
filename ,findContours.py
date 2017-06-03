b88b7fd42e0e1454796320da2760bcf826104003
山本憲 <em072029@gmail.com>
2017-06-03 17:26:33 +0900

    色の付け方終了

--- ,findContours.py
+++ ,findContours.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import cv2
 
 img = cv2.imread("./test2.png")
@@ -7,8 +8,8 @@ gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 ret,hierarchy = cv2.findContours(gray, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE )
 # print("ret=",len(ret),  "hierarchy=",len(hierarchy) )
 
-print(ret[0][0])
-print(ret[0][1])
+# print(ret[0][0])
+# print(ret[0][1])
 # print(len(ret[0][0]))
 
 cv2.drawContours(img, ret[0], -1,(255,  0,  0),3)
@@ -18,4 +19,11 @@ cv2.drawContours(img, ret[3], -1,(255,255,  0),3)
 cv2.drawContours(img, ret[4], -1,(  0,255,255),3)
 cv2.drawContours(img, ret[5], -1,(255,  0,255),3)
 cv2.drawContours(img, ret[6], -1,(  0,  0,  0),3)
+cv2.imshow('image', img)
+cv2.waitKey(0)
 
+if key == 27:            #u'escの処理'
+    cv2.destroyAllWindows()   
+elif k == ord('s'):      #u'sの入力の処理''
+    cv2.imwrite('forsale.jpg', img)
+    cv2.destroyAllWindows()
